375dad8af01abcc70aba426a83619a1def04b75c
MaxFunk <funk-masik@yandex.ru>
2023-04-29 22:46:04 +0300

    inicial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+
+        post.birthdey = new FormDate();
+
+        post.birthdey.day = 13;
+        post.birthdey.month = 6;
+        post.birthdey.year = 1999;
+
+    }
+}
+
+
+
